70fa7e6678eb2ab0daae40f6d0289d09a2146e55
unknown <alsrn6040@naver.com>
2022-03-17 10:37:54 +0900

    회원가입 라우팅 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -11,14 +11,26 @@ const output = {
     login: (req, res) => {
         res.render("home/login");
     },
+
+    register: (req,res) => {
+        res.render("home/register");
+    }
 };
 
 const process = {
-     login: (req,res) => {
+     
+    
+    login: (req,res) => {
         const user = new User(req.body);
         const response = user.login();
         return res.json(response);
     },
+
+    register: (req,res) => {
+        const user = new User(req.body);
+        const response = user.register();
+        return res.json(response);
+    }
  };
 
 module.exports = {
